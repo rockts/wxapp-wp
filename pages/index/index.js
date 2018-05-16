b0757031a44c903a17b9fde8e1e2eb32e9c972cf
rockts <rockts@sina.com>
2018-05-17 01:06:37 +0800

    加载烈被分页

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,8 +1,51 @@
 const app = getApp()
 
+const API_BASE = 'https://sandbox.lekee.cc/wp-json'
+const API_ROUTE = 'wp/v2/posts'
+
 Page({
   data: {
+    entities: [],
+    embed: true,
+    total: 0,
+    totalPages: 0,
+    currentPage: 1
   },
   onLoad () {
+    wx.request({
+      url: `${ API_BASE }/${ API_ROUTE }?_embed=${ this.data.embed }`,
+      success: (response) => {
+        console.log(response)
+        const entities = response.data
+        this.setData({
+          entities,
+          total: response.header['x-wp-total'],
+          totalPages: response.header['x-wp-totalpages']
+        })
+      }
+    })
+  },
+  onReachBottom () {
+    let { currentPage, totalPages } = this.data
+
+    if (currentPage >= totalPages) {
+      return
+    }
+
+    currentPage = currentPage + 1
+
+    wx.request({
+      url: `${ API_BASE }/${ API_ROUTE }?_embed=${ this.data.embed }&page=${ currentPage }`,
+      success: (response) => {
+        console.log(response)
+        const entities = [...this.data.entities, ...response.data]
+        this.setData({
+          entities,
+          currentPage,
+          total: response.header['x-wp-total'],
+          totalPages: response.header['x-wp-totalpages']
+        })
+      }
+    })
   }
 })
