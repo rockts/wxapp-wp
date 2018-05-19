7981fe496a94b2f741f7c54c74be8af442d2f060
rockts <rockts@sina.com>
2018-05-20 00:43:28 +0800

    完成《微信小程序：内容评论 #8》

--- pages/posts/show.js
+++ pages/posts/show.js
@@ -1,7 +1,13 @@
 import {
   API_BASE,
-  API_ROUTE_POSTS
+  API_ROUTE_POSTS,
+  API_ROUTE_COMMENTS
 } from '../../config/api'
+
+import moment from '../../vendor/moment/moment'
+import '../../vendor/moment/locale/zh-cn'
+moment.locale('zh-cn')
+
 
 const app = getApp()
 const { towxml } = app
@@ -12,12 +18,64 @@ Page({
     content: {},
     featured_media: '',
     author: {},
-    isLoading: true
+    isLoading: true,
+    comments: []
   },
   onLoad (options) {
-    const id = options.id
-    // const id = 31
+    // const id = options.id
+    const id = 57
 
+    this.getPost(id)
+    this.getComments(id)
+  },
+  transformComments (comments) {
+    return comments.map((item) => {
+      const content = {
+        ...item.content,
+        wxml: towxml.toJson(item.content.rendered, 'html')
+      }
+
+      const fromNow = moment.utc(item.date).local().fromNow()
+
+      let comment = {
+        ...item,
+        content,
+        fromNow
+      }
+
+      if (item.parent !== 0) {
+        const inReplyTo = item._embedded['in-reply-to'][0]
+
+        const reply = {
+          ...inReplyTo,
+          wxml: towxml.toJson(inReplyTo.content.rendered, 'html'),
+          fromNow: moment.utc(inReplyTo.date).local().fromNow()
+        }
+
+        comment = {
+          ...comment,
+          reply
+        }
+      }
+
+      return comment
+    })
+  },
+  getComments (postId) {
+    wx.request({
+      url: `${ API_BASE }/${ API_ROUTE_COMMENTS }?post=${ postId }&_embed=true`,
+      success: (response) => {
+        console.log(response)
+        const comments = this.transformComments(response.data)
+        if (comments.length > 0) {
+          this.setData({
+            comments
+          })
+        }
+      }
+    })
+  },
+  getPost (id) {
     wx.request({
       url: `${ API_BASE }/${ API_ROUTE_POSTS }/${ id }?_embed=true`,
       success: (response) => {
