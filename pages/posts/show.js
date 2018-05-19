115638c2ce988461ad328997dd6d4271b28647f7
rockts <rockts@sina.com>
2018-05-20 05:04:25 +0800

    完成《微信小程序：评论功能 #9》

--- pages/posts/show.js
+++ pages/posts/show.js
@@ -4,6 +4,7 @@ import {
   API_ROUTE_COMMENTS
 } from '../../config/api'
 
+import { validateToken } from '../../libs/auth-jwt'
 import moment from '../../vendor/moment/moment'
 import '../../vendor/moment/locale/zh-cn'
 moment.locale('zh-cn')
@@ -19,14 +20,99 @@ Page({
     featured_media: '',
     author: {},
     isLoading: true,
-    comments: []
+    comments: [],
+    total: 0,
+    totalPage: 0,
+    currentPage: 1,
+    isEarth: false,
+    comment: {},
+    placeholder: '',
+    focus: false,
+    jwt: {},
+    loggedIn: true
   },
   onLoad (options) {
-    // const id = options.id
-    const id = 57
+    const id = options.id
+    // const id = 57
+
+    const { jwt } = app.globalData
+
+    validateToken(jwt, (result) => {
+      if (!result) {
+        this.setData({
+          loggedIn: false
+        })
+      }
+    })
+
+    this.setData({
+      jwt
+    })
 
     this.getPost(id)
     this.getComments(id)
+
+    setInterval(() => {
+      const comments = this.transformComments(this.data.comments)
+
+      this.setData({
+        comments
+      })
+    }, 1000*60)
+  },
+  onInputComment (event) {
+    this.setData({
+      ['comment.content']: event.detail.value
+    })
+  },
+  onSubmitComment () {
+    wx.request({
+      url: `${ API_BASE }/${ API_ROUTE_COMMENTS }`,
+      method: 'POST',
+      data: {
+        ...this.data.comment,
+        post: this.data.id,
+
+      },
+      header: {
+        'Authorization': `Bearer ${ this.data.jwt.token }`
+      },
+      success: (response) => {
+        if (response.statusCode === 201) {
+          this.getComments(this.data.id)
+          this.setData({
+            comment: {}
+          })
+        }
+      }
+    })
+  },
+  onTapLoginButton () {
+    wx.switchTab({
+      url: '/pages/users/show'
+    })
+  },
+  onBlurComment () {
+    this.setData({
+      focus: false,
+      placeholder: '',
+      ['comment.parent']: 0
+    })
+  },
+  onTapComment (event) {
+    this.setData({
+      focus: true,
+      placeholder: `回复：${ event.currentTarget.dataset.author }`,
+      ['comment.parent']: event.currentTarget.dataset.id
+    })
+  },
+  onPullDownRefresh () {
+    const id = this.data.id
+    this.getPost(id, () => {
+      this.getComments(id, () => {
+        wx.stopPullDownRefresh()
+      })
+    })
   },
   transformComments (comments) {
     return comments.map((item) => {
@@ -61,21 +147,74 @@ Page({
       return comment
     })
   },
-  getComments (postId) {
+  getComments (postId, callback = () =>{
+
+  }) {
     wx.request({
       url: `${ API_BASE }/${ API_ROUTE_COMMENTS }?post=${ postId }&_embed=true`,
       success: (response) => {
         console.log(response)
         const comments = this.transformComments(response.data)
         if (comments.length > 0) {
+          const total = response.header['X-WP-Total'] || response.header['x-wp-total']
+          const totalPages = response.header['X-WP-TotalPages'] || response.header['x-wp-totalpages']
+
+          let isEarth = false
+          if (totalPages == 1) {
+            isEarth = true
+          }
+
           this.setData({
-            comments
+            comments,
+            isLoading: false,
+            total,
+            totalPages,
+            currentPage: 1,
+            isEarth
           })
         }
+
+        callback(response)
       }
     })
   },
-  getPost (id) {
+  onReachBottom () {
+    let { currentPage, totalPages, isLoading } = this.data
+
+    if (currentPage >= totalPages || isLoading) {
+      return
+    }
+
+    this.setData({
+      isLoading: true
+    })
+
+    currentPage = currentPage + 1
+
+    wx.request({
+      url: `${ API_BASE }/${ API_ROUTE_COMMENTS }?_embed=true&page=${ currentPage }&post=${ this.data.id }`,
+      success: (response) => {
+        let comments = this.transformComments(response.data)
+
+        comments = [...this.data.comments, ...comments]
+
+        const total = response.header['X-WP-Total'] || response.header['x-wp-total']
+        const totalPages = response.header['X-WP-TotalPages'] || response.header['x-wp-totalpages']
+
+        this.setData({
+          comments,
+          currentPage,
+          isLoading: false,
+          total,
+          totalPages,
+          isEarth: currentPage >= totalPages
+        })
+      }
+    })
+  },
+  getPost (id, callback = () => {
+
+  }) {
     wx.request({
       url: `${ API_BASE }/${ API_ROUTE_POSTS }/${ id }?_embed=true`,
       success: (response) => {
@@ -99,6 +238,8 @@ Page({
         wx.setNavigationBarTitle({
           title
         })
+
+        callback(response)
       }
     })
   }
